b194976c9113cce9bb0fee5c91dbaaf8a4841db3
Santiago Morillo Segovia <santiagomorillosegovia@gmail.com>
2021-10-14 01:43:47 -0500

    DONE

--- routes/dog.py
+++ routes/dog.py
@@ -16,56 +16,48 @@ dog = APIRouter()
 
 @dog.get('/api/dogs', tags=["dogs"])
 def show_dogs():
-   
-   return dogsEntity(db.dogs.find())
-   
 
+    return dogsEntity(db.dogs.find())
 
 
 @dog.get('/api/dogs/is_adopted', tags=["dogs"])
 def show_adopted():
-   return dogsEntity(db.dogs.find( { "is_adopted": True }))
+    return dogsEntity(db.dogs.find({"is_adopted": True}))
 
 
 @dog.get('/api/dogs/{name}', tags=["dogs"])
 def show_dog(name: str):
-    return dogsEntity(db.dogs.find( { "name": name } ))
+    return dogsEntity(db.dogs.find({"name": name}))
 
 
-
-@dog.post('/api/dogs/{name}', dependencies=[Depends(JWTBearer())],tags=["dogs"])
+@dog.post('/api/dogs/{name}', dependencies=[Depends(JWTBearer())], tags=["dogs"])
 def create_dog(dog: Dog):
     new_dog = dict(dog)
-    del new_dog ["create_date"]
+    del new_dog["create_date"]
     del new_dog["picture"]
 
-
     image = requests.get('https://dog.ceo/api/breeds/image/random')
-    image=image.json()
-    new_dog ["picture"]= image["message"]
-   
-    new_dog ["create_date"]= datetime.now()
+    image = image.json()
+    new_dog["picture"] = image["message"]
+
+    new_dog["create_date"] = datetime.now()
     db.dogs.insert_one(new_dog)
     return "Dog Saved"
-
 
 
 @dog.put('/api/dogs/{name}', tags=["dogs"])
 def edit_dog(name: str, dog: Dog):
     new_dog = dict(dog)
-    del new_dog ["create_date"]
-    del new_dog ["picture"]
-    db.dogs.find_one_and_update({ "name": name },{"$set": new_dog})
+    del new_dog["create_date"]
+    del new_dog["picture"]
+    db.dogs.find_one_and_update({"name": name}, {"$set": new_dog})
     return "Dog Updated"
-
 
 
 @dog.delete('/api/dogs/{name}', tags=["dogs"])
 def delete_dog(name: str):
-    db.dogs.find_one_and_delete({ "name": name })
+    db.dogs.find_one_and_delete({"name": name})
     return "Dog Deleted"
-
-
 
 
 ######################################
@@ -75,19 +67,15 @@ def delete_dog(name: str):
 async def create_user(user: UserSchema = Body(...)):
     new_user = dict(user)
     db.users.insert_one(new_user)
-    #users.append(user) # replace with db call, making sure to hash the password first
     return signJWT(user.email)
-
 
 
 def check_user(data: UserLoginSchema):
     users = usersEntity(db.users.find())
-    #return users[1]['email']
     for user in users:
         if user['email'] == data.email and user['password'] == data.password:
             return True
     return False
-
 
 
 @dog.post("/api/user/login", tags=["user"])
@@ -101,25 +89,25 @@ async def user_login(user: UserLoginSchema = Body(...)):
 
 @dog.get('/api/user', tags=["user"])
 def show_users():
-   
-   return usersEntity(db.users.find())
-   
+
+    return usersEntity(db.users.find())
+
 
 @dog.get('/api/user/{id}', tags=["user"])
 def show_user(id: str):
-    return usersEntity(db.users.find( { "_id": ObjectId(id) } ))
+    return usersEntity(db.users.find({"_id": ObjectId(id)}))
 
 
 @dog.put('/api/user/{id}', tags=["user"])
 def edit_user(id: str, user: UserSchema):
     new_user = dict(user)
-    db.users.find_one_and_update({ "_id": ObjectId(id) },{"$set": new_user})
+    db.users.find_one_and_update({"_id": ObjectId(id)}, {"$set": new_user})
     return "User Updated"
 
 
 @dog.delete('/api/user/{id}', tags=["user"])
 def delete_user(id: str):
-    db.users.find_one_and_delete({ "_id": ObjectId(id) })
+    db.users.find_one_and_delete({"_id": ObjectId(id)})
     return "User Deleted"
 
 
@@ -127,11 +115,14 @@ def delete_user(id: str):
 
 @dog.post('/api/file', tags=["file"])
 async def upload_file(file: UploadFile = File(...)):
-    
-    new_file1 = await file.read()
-    
-    new_file = {'file': file.file}
-    
-    file_response = requests.post('https://gttb.guane.dev/api/files', files=new_file)
- 
-    return new_file
+
+    file_content = await file.read()
+
+    files = [
+        ('file', (file.filename, file_content, file.content_type))
+    ]
+
+    file_response = requests.post(
+        'https://gttb.guane.dev/api/files', files=files)
+
+    return file_response.json()
--- security/model.py
+++ security/model.py
@@ -1,18 +1,13 @@
 from pydantic import BaseModel, Field, EmailStr
 
 
-
 class UserSchema(BaseModel):
     name: str = Field(...)
     last_name: str = Field(...)
     email: EmailStr = Field(...)
     password: str = Field(...)
 
-    
 
 class UserLoginSchema(BaseModel):
     email: EmailStr = Field(...)
     password: str = Field(...)
-
-   
-
